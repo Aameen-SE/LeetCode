c25941b1e3f9ae997677d7d2ff9e3c8e9aee8e81
Aameen-SE <aameen.se@gmail.com>
2025-09-26 15:11:08 +0530

    Longest SubString without Repeating Character

---
+++ LongestSubstringWithoutRepeating.java
@@ -0,0 +1,28 @@
+import java.util.*;
+//Longest Substring Without Repeating
+public class LongestSubstringWithoutRepeating {
+    public static int lengthOfLongestSubstring(String s) {
+        Set<Character> set = new HashSet<>();
+        int left = 0, maxLen = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            char c = s.charAt(right);
+
+
+            while (set.contains(c)) {
+                set.remove(s.charAt(left));
+                left++;
+            }
+
+            set.add(c);
+            maxLen = Math.max(maxLen, right - left + 1);
+        }
+        return maxLen;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(lengthOfLongestSubstring("abcabcbb"));
+        System.out.println(lengthOfLongestSubstring("bbbbb"));
+        System.out.println(lengthOfLongestSubstring("pwwkew"));
+    }
+}
